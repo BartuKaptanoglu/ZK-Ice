b576486d08c3c36c3c2d0aef5153955c78276942
BartuKaptanoglu <bartu.kaptanoglu@gmail.com>
2024-02-09 20:52:00 +0300

    Amil_Bodes

--- contracts/Proposal.sol
+++ contracts/Proposal.sol
@@ -1,32 +1,39 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.10 <0.9.0;
-
-contract DAOVoting{
-   Proposal[] public proposals;
-   uint256 id=0;
-   struct Proposal{
-    uint256 deadline;
-    uint256 yesVotes;
-    uint256 noVotes;
-    uint256 proposalID;
-    uint256 nonCommitalVotes;
-    string proposalTitle;
-    string proposalBody; 
-    bool executed;
-   }
-   enum VOTES{
-      YES, ///1
-      NO, ///-1
-      MAYBE ///0
-   }
-   mapping(uint256 => mapping(uint256 => bool)) votersForEachProposal;
-   function createProposal(string memory _proposalTitle, string memory _proposalBody) public {
-      Proposal(0,0,0,id,0,_proposalTitle,_proposalBody,false);
-      id++;
-   }
-   function submitProposal(Proposal storage proposalTemplate) internal {
-      proposalTemplate.deadline=block.timestamp+5 minutes;
-      proposals.push(proposalTemplate);
-   }
-  
-}
+///SPDX-License-Identifier: MIT
+ pragma solidity >=0.8.10 <0.9.0;
+ contract DAOVoting{ 
+   Proposal[] public proposals; 
+ Proposal[] public proposalsQueForExec; 
+ uint256 id=0;
+  struct user{ 
+   uint uID;
+   string name; 
+   } struct 
+   Proposal{ 
+      uint256 deadline; 
+      uint256 proposalID; 
+      int256 votes; 
+      string proposalBody; 
+      bool executed; } 
+      mapping(uint256 => mapping (uint256 => bool)) votersForEachProposal; 
+      function createProposal( string memory _proposalBody) public 
+      { Proposal(0,id,0,_proposalBody,false); id++; } 
+      function submitProposal(Proposal storage proposalTemplate) internal {
+          ///TODO: SEND PROPOSAL 
+          proposalTemplate.deadline=block.timestamp+5 minutes; 
+          proposals.push(proposalTemplate); 
+      } function executeProposal(uint256 _proposalID) public{ 
+            for (uint i = 0; i < proposals.length; i++)
+            { if(proposals[i].proposalID==_proposalID){ 
+               proposals[i] = proposals[proposals.length - 1]; 
+               proposalsQueForExec.push( proposals[i]); proposals.pop(); } } 
+               ///TODO: IMPLEMENT PROPOSAL 
+               } 
+      function voteOnProposal(uint _uID ,uint256 _proposalID, uint8 vote) internal{ 
+         require(votersForEachProposal[_proposalID][_uID]==false,"YOU CAN'T VOTE ON THIS"); 
+         for (uint i = 0; i < proposals.length; i++){
+             if(proposals[i].proposalID==_proposalID){
+                proposals[i].proposalID+=vote; votersForEachProposal[_proposalID][_uID]=true; 
+                } 
+                }
+                 }
+ }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,28 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    scrollSepolia: {
+      url: "https://sepolia-rpc.scroll.io/" || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      scrollSepolia: 'abc',
+    },
+    customChains: [
+      {
+        network: 'scrollSepolia',
+        chainId: 534351,
+        urls: {
+          apiURL: 'https://sepolia-blockscout.scroll.io/api',
+          browserURL: 'https://sepolia-blockscout.scroll.io/',
+        },
+      },
+    ],
+  },
 };
 
 export default config;
